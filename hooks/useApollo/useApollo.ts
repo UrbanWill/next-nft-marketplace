ede3a714ff26b7016e94a20bf7e1b73cb3b6c038
UrbanWill <willian.urban@gmail.com>
2022-10-27 18:10:56 +0700

    setup apollo client

---
+++ hooks/useApollo/useApollo.ts
@@ -0,0 +1,100 @@
+import { useMemo } from "react";
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  from,
+  NormalizedCacheObject,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { onError } from "@apollo/client/link/error";
+import type { AppProps } from "next/app";
+import merge from "deepmerge";
+
+export const APOLLO_STATE_PROP_NAME = "__APOLLO_STATE__";
+
+let apolloClient: ApolloClient<NormalizedCacheObject> | undefined;
+
+const uri = process.env.NFT_API_URL || "http://localhost:4000";
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const httpLink = new HttpLink({
+  uri, // Server URL (must be absolute)
+  credentials: "same-origin", // Additional fetch() options like `credentials` or `headers`
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+function createApolloClient() {
+  return new ApolloClient({
+    ssrMode: typeof window === "undefined",
+    link: authLink.concat(from([errorLink, httpLink])),
+    cache: new InMemoryCache(),
+  });
+}
+
+type InitialState = NormalizedCacheObject | undefined;
+
+interface IInitializeApollo {
+  initialState?: InitialState | null;
+}
+
+export function initializeApollo(initialState: IInitializeApollo) {
+  const _apolloClient = apolloClient ?? createApolloClient();
+
+  // If your page has Next.js data fetching methods that use Apollo Client, the initial state
+  // get hydrated here
+  if (initialState) {
+    // Get existing cache, loaded during client side data fetching
+    const existingCache = _apolloClient.extract();
+
+    // Merge the existing cache into data passed from getStaticProps/getServerSideProps
+    const data = merge(initialState, existingCache);
+
+    // Restore the cache with the merged data
+    _apolloClient.cache.restore(data);
+  }
+  // For SSG and SSR always create a new Apollo Client
+  if (typeof window === "undefined") {
+    return _apolloClient;
+  }
+  // Create the Apollo Client once in the client
+  if (!apolloClient) apolloClient = _apolloClient;
+
+  return _apolloClient;
+}
+
+export function addApolloState(
+  client: ApolloClient<NormalizedCacheObject>,
+  pageProps: AppProps["pageProps"]
+) {
+  if (pageProps?.props) {
+    pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract();
+  }
+
+  return pageProps;
+}
+
+export function useApollo(initialState: AppProps["pageProps"]) {
+  const store = useMemo(() => initializeApollo(initialState), [initialState]);
+  return store;
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,23 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/client";
+
+// hooks
+import { useApollo } from "../hooks/useApollo/useApollo";
 
 // components
 import { Layout } from "../components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
   return (
     <ChakraProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ApolloProvider client={apolloClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
     </ChakraProvider>
   );
 }
