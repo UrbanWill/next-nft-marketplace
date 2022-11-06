5109a9a71ccf39b650756f4ec569d4c7e6fbd882
UrbanWill <willian.urban@gmail.com>
2022-11-06 21:50:34 +0700

    installed and configure dotenv

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+require("dotenv").config();
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    NFT_API_URL: process.env.NFT_API_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
