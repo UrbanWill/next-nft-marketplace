00145e88f95c9e3e2d24fff22a4922fc6b08962f
UrbanWill <willian.urban@gmail.com>
2023-01-01 20:45:17 +0700

    installed and config react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import { useRouter } from "next/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // hooks
 import { useApollo } from "../hooks/useApollo";
@@ -17,21 +18,25 @@ export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
   const { pathname } = useRouter();
 
+  const queryClient = new QueryClient();
+
   return (
     <ChakraProvider>
-      <ApolloProvider client={apolloClient}>
-        <AuthProvider>
-          <Layout>
-            {noAuthRequired.includes(pathname) ? (
-              <Component {...pageProps} />
-            ) : (
-              <ProtectedRoute>
+      <QueryClientProvider client={queryClient}>
+        <ApolloProvider client={apolloClient}>
+          <AuthProvider>
+            <Layout>
+              {noAuthRequired.includes(pathname) ? (
                 <Component {...pageProps} />
-              </ProtectedRoute>
-            )}
-          </Layout>
-        </AuthProvider>
-      </ApolloProvider>
+              ) : (
+                <ProtectedRoute>
+                  <Component {...pageProps} />
+                </ProtectedRoute>
+              )}
+            </Layout>
+          </AuthProvider>
+        </ApolloProvider>
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
