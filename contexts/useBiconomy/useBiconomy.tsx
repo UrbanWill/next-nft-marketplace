f0e1e8c8e8d362a67ff254e5af73e02cd8e7c78f
UrbanWill <willian.urban@gmail.com>
2023-03-20 10:59:41 +0700

    created biconomy context

---
+++ contexts/useBiconomy/useBiconomy.tsx
@@ -0,0 +1,63 @@
+import {
+  useState,
+  useEffect,
+  createContext,
+  useContext,
+  ReactNode,
+} from "react";
+import { Biconomy } from "@biconomy/mexa";
+
+// hooks
+import { useAuth } from "../../contexts/useAuth";
+
+// types
+import { ExternalProvider } from "../../utils/types";
+
+// constants
+import {
+  BICONOMY_API_KEY,
+  NFT_MARKETPLACE_CONTRACT_ADDRESS_EIP_712,
+} from "../../utils/constants";
+
+interface IBiconomyContext {
+  biconomy: Biconomy | null;
+}
+
+export const BiconomyContext = createContext<IBiconomyContext>({
+  biconomy: null,
+});
+
+export function BiconomyProvider({
+  children,
+}: {
+  children: ReactNode;
+}): JSX.Element {
+  const [biconomy, setBiconomy] = useState<Biconomy | null>(null);
+  const { isAuthenticated } = useAuth();
+
+  useEffect(() => {
+    const initBiconomy = async () => {
+      const biconomy = new Biconomy(window.ethereum as ExternalProvider, {
+        apiKey: BICONOMY_API_KEY,
+        debug: true,
+        contractAddresses: [NFT_MARKETPLACE_CONTRACT_ADDRESS_EIP_712],
+      });
+      await biconomy.init();
+      setBiconomy(biconomy);
+    };
+    if (isAuthenticated) {
+      initBiconomy();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated]);
+
+  return (
+    <BiconomyContext.Provider value={{ biconomy }}>
+      {children}
+    </BiconomyContext.Provider>
+  );
+}
+
+export default function useBiconomy() {
+  return useContext(BiconomyContext);
+}
