d7943fcd50d699729155a2516a5d04e1db3901a7
UrbanWill <willian.urban@gmail.com>
2023-03-21 20:47:49 +0700

    created useRequestApproval hook

---
+++ hooks/mutations/useRequestApproval/index.ts
@@ -0,0 +1,1 @@
+export { useRequestApproval } from "./useRequestApproval";
---
+++ hooks/mutations/useRequestApproval/useRequestApproval.ts
@@ -0,0 +1,50 @@
+import { ethers, Contract } from "ethers";
+import { useMutation } from "@tanstack/react-query";
+import { useContract, useSigner } from "wagmi";
+
+// abis
+import BasicNft from "../../../contracts/abis/BasicNft.json";
+import NftMarketplace from "../../../contracts/abis/NftMarketplace.json";
+
+// constants
+import { MATIC_RPC_URL, MATIC_NETWORK_ID } from "../../../utils/constants";
+
+const provider = new ethers.providers.JsonRpcProvider(
+  MATIC_RPC_URL,
+  MATIC_NETWORK_ID
+);
+
+const fetchRequestApproval = async ({
+  contract,
+  id,
+}: {
+  contract: Contract;
+  id: string;
+}) => {
+  const approvalRequest = await contract.approve(
+    NftMarketplace.contract.address,
+    id
+  );
+  return approvalRequest;
+};
+
+export const useRequestApproval = ({
+  tokenContractAddress,
+}: {
+  tokenContractAddress: string;
+}) => {
+  const { data: signer } = useSigner();
+  const BasicNftContract = useContract({
+    address: tokenContractAddress,
+    abi: BasicNft.contract.abi,
+    signerOrProvider: signer,
+  }) as Contract;
+
+  const requestApproval = useMutation({
+    mutationFn: ({ id }: { id: string }) => {
+      return fetchRequestApproval({ contract: BasicNftContract, id });
+    },
+  });
+
+  return { requestApproval };
+};
